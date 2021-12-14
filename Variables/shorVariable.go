b2b02214bff8d67d25c1447d117b2c4211c16875
Muhammad Rezaul Karim <77927449+irezaul@users.noreply.github.com>
2021-12-14 17:00:56 +0600

    Verbs Print

--- Variables/shorVariable.go
+++ Variables/shorVariable.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	// short variable method -1
-	x := 10
-	fmt.Println(x)
+	// x := 10
+	// fmt.Println(x)
 
+	// Method 2 ( verbs print)
+	x := 10
+	fmt.Printf("%T", x) // %T - Type of value
 }
